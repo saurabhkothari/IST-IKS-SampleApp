9d860c4f37614be39ae9212afd19acb5a4439465
saurabhkothari <saurabhkothari@rocketmail.com>
2022-02-18 14:19:38 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,33 @@
+terraform {
+  required_providers {
+    helm = {
+      source = "hashicorp/helm"
+      version = "2.4.1"
+    }
+  }
+}
+
+resource helm_release nginx_ingress {
+  name       = "nginx-ingress-controller"
+
+  repository = "https://charts.bitnami.com/bitnami"
+  chart      = "nginx-ingress-controller"
+
+  set {
+    name  = "service.type"
+    value = "ClusterIP"
+  }
+}
+
+provider "helm" {
+  kubernetes {
+    host = local.kube_config.clusters[0].cluster.server
+    client_certificate = base64decode(local.kube_config.users[0].user.client-certificate-data)
+    client_key = base64decode(local.kube_config.users[0].user.client-key-data)
+    cluster_ca_certificate = base64decode(local.kube_config.clusters[0].cluster.certificate-authority-data)
+  }
+}
+
+locals {
+  kube_config = yamldecode(var.kube_config)
+}
